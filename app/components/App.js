8f9e7564066d21f12af6c810a56bd7f488188ee6
Leonardo Cardoso <leonardo.cardoso@one.com.br>
2017-07-13 15:48:19 -0300

    Axios, Promises, and the Github API

--- app/components/App.js
+++ app/components/App.js
@@ -9,25 +9,25 @@ var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Switch = ReactRouter.Switch;
 
-class App extends React.Component{
-	render() {
-		return(
-			<Router>
-			<div className='container'>
-			<Nav/>
-			<Switch>
-			<Route exact path="/" component={Home} />
-			<Route exact path='/battle' component={Battle} />
-      <Route path='/battle/results' component={Results} />
-			<Route path='/popular' component={Popular} />
-			<Route render={function () {
-				return <p>Not Found</p>
-			}}/>
-			</Switch>
-			</div>
-			</Router>
-			)
-	}
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 module.exports = App;
